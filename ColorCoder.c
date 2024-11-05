7098dcb35e15793fc2293b365037c97257b3ce68
kumrdpk123 <kumrdpk123@gmail.com>
2024-11-05 21:47:17 +0530

    Update ColorCoder.c

--- ColorCoder.c
+++ ColorCoder.c
@@ -4,11 +4,13 @@
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
+
 const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
+
 const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
