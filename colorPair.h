1240af8b1a8d6533011e24146a1ff6a227105837
kumrdpk123 <kumrdpk123@gmail.com>
2024-11-05 22:02:32 +0530

    Update and rename ColorCoder.h to colorPair.h

---
+++ colorPair.h
@@ -0,0 +1,18 @@
+#ifndef COLOR_PAIR_H
+#define COLOR_PAIR_H
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+void printColorCodeReferenceManual(int numberOfMajorColors, int numberOfMinorColors);
+#endif // COLOR_PAIR_H
